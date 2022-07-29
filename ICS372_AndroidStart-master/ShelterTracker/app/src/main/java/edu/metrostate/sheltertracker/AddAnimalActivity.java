2aefc91662db34f12d577c1237339458959fc13f
KiyokoNee <k.c.gearing@gmail.com>
2022-07-29 18:36:04 -0500

    Rough attempt at activity_add_animal.xml

--- ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/AddAnimalActivity.java
+++ ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/AddAnimalActivity.java
@@ -1,6 +1,7 @@
 package edu.metrostate.sheltertracker;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +13,15 @@ public class AddAnimalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shelter_list);
+        setContentView(R.layout.activity_add_animal);
 
         ListView lv = findViewById(R.id.shelter_list);
 
         lv.setAdapter(new ShelterAdapter(this,
                 (List<Shelter>) ((ShelterTrackerApplication)getApplication()).getShelterList().getShelters()));
 
+    }
+
+    public void onRadioButtonClicked(View view) {
     }
 }
