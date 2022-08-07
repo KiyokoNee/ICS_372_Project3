3ca2c896a849b409184e7cb6a0f869fed001e027
kongthao <kong.thao100@gmail.com>
2022-08-07 18:13:45 -0500

    Added dialogs/pop up to the AddAnimalActivity based on the condition if the animal was successfully added or not.

--- ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/AddAnimalActivity.java
+++ ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/AddAnimalActivity.java
@@ -1,11 +1,15 @@
 package edu.metrostate.sheltertracker;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -16,6 +20,7 @@ import java.util.List;
 
 
 public class AddAnimalActivity extends AppCompatActivity {
+    private RadioGroup radioGroup = null;
     private RadioButton radioLB = null;
     private RadioButton radioKG = null;
     private String animal_Type;
@@ -33,6 +38,7 @@ public class AddAnimalActivity extends AppCompatActivity {
         shelterArrayList.addAll(shelters);
         radioLB = findViewById(R.id.radio_lb);
         radioKG = findViewById(R.id.radio_kg);
+        radioGroup = findViewById(R.id.radio_group);
         fillTypeSpinner();
         fillShelterSpinner();
 
@@ -52,16 +58,52 @@ public class AddAnimalActivity extends AppCompatActivity {
 
         EditText mEdit = findViewById(R.id.nameInput);
         String animal_Name = mEdit.getText().toString();
+        mEdit.getText().clear();
         mEdit = findViewById(R.id.idInput);
         String animal_ID = mEdit.getText().toString();
+        mEdit.getText().clear();
         mEdit = findViewById(R.id.animalWeightInput);
         double animal_weight = Double.valueOf(mEdit.getText().toString());
+        mEdit.getText().clear();
         mEdit = findViewById(R.id.receipt_date);
         long receipt_date = Long.valueOf(mEdit.getText().toString());
+        mEdit.getText().clear();
 
-        Animal newAnimal = new Animal(animal_Type, animal_Name, animal_ID, animal_weight, weight_unit, receipt_date);
-        Shelter.addUserCreatedAnimal(newAnimal, shelter.getShelterID(), (((ShelterTrackerApplication)getApplication()).getShelterList()));
+        Dialog dialog;
+        if(shelter.isReceiving()){
+            Animal newAnimal = new Animal(animal_Type, animal_Name, animal_ID, animal_weight, weight_unit, receipt_date);
+            Shelter.addUserCreatedAnimal(newAnimal, shelter.getShelterID(), (((ShelterTrackerApplication)getApplication()).getShelterList()));
 
+            if(shelter.getAnimalList().contains(newAnimal)){
+                dialog = new AlertDialog.Builder(this).setTitle("Adding Animal Status").setCancelable(false)
+                        .setMessage(newAnimal.getAnimal_Name() + " was successfully added to shelter " + shelter.getShelterName())
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }).create();
+
+            }else{
+                dialog = new AlertDialog.Builder(this).setTitle("Adding Animal Status").setCancelable(false)
+                        .setMessage(newAnimal.getAnimal_Name() + " was not successfully added to shelter " + shelter.getShelterName())
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }).create();
+            }
+            dialog.show();
+        }else{
+            dialog = new AlertDialog.Builder(this).setTitle("Adding Animal Status").setCancelable(false)
+                    .setMessage("Receiving Status: " +  shelter.isReceiving())
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    }).create();
+        }
+        dialog.show();
+        radioGroup.clearCheck();
     }
 
     public void fillTypeSpinner(){
