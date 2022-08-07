a7759725cb00be36547c5d812c6f7c5f739ea9ad
walrusOperator <indierevolver@gmail.com>
2022-08-07 12:07:00 -0500

    Started work on import export wiring

--- ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ImportJSONActivity.java
+++ ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ImportJSONActivity.java
@@ -8,16 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.List;
 
 public class ImportJSONActivity extends AppCompatActivity {
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shelter_list);
+        setContentView(R.layout.import_shelter);
 
-        ListView lv = findViewById(R.id.shelter_list);
+//        ListView lv = findViewById(R.id.shelter_list);
 
-        lv.setAdapter(new ShelterAdapter(this,
-                (List<Shelter>) ((ShelterTrackerApplication)getApplication()).getShelterList().getShelters()));
-
+//        lv.setAdapter(new ShelterAdapter(this,
+//                (List<Shelter>) ((ShelterTrackerApplication)getApplication()).getShelterList().getShelters()));
+//
     }
 }
