b78b0f966e83d1981cdcc03278312fc4032fadc0
walrusOperator <indierevolver@gmail.com>
2022-08-07 12:54:37 -0500

    All layout displaying; wiring functionality

--- ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ImportFileActivity.java
+++ ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ImportFileActivity.java
@@ -1,6 +1,9 @@
 package edu.metrostate.sheltertracker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,20 @@ public class ImportFileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.import_shelter);
+            ShelterList shelterList = (((ShelterTrackerApplication)getApplication()).getShelterList());
+
+
+        final Button button = (Button) findViewById(R.id.submit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText mEdit = findViewById(R.id.fileNameInput);
+                String file_name = mEdit.getText().toString();
+                String localFileLocation = getExternalFilesDir(null).getAbsolutePath()+file_name;
+                shelterList.addHashMap(ParseUtilities.loadJSON(localFileLocation));
+                ((ShelterTrackerApplication)getApplication()).writeFile();
+            }
+        });
 
 //        ListView lv = findViewById(R.id.shelter_list);
 
