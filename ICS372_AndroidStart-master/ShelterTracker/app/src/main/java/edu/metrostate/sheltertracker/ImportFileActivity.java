583f02662de764a65011992e722c22980865222f
walrusOperator <indierevolver@gmail.com>
2022-08-07 13:06:05 -0500

    Attempting to add file validation to ImportFileActivity

--- ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ImportFileActivity.java
+++ ICS372_AndroidStart-master/ShelterTracker/app/src/main/java/edu/metrostate/sheltertracker/ImportFileActivity.java
@@ -1,5 +1,8 @@
 package edu.metrostate.sheltertracker;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +11,7 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
 import java.util.List;
 
 public class ImportFileActivity extends AppCompatActivity {
@@ -27,16 +31,34 @@ public class ImportFileActivity extends AppCompatActivity {
                 EditText mEdit = findViewById(R.id.fileNameInput);
                 String file_name = mEdit.getText().toString();
                 String localFileLocation = getExternalFilesDir(null).getAbsolutePath()+file_name;
+                File file = new File(localFileLocation);
+                if(!file.exists()){
+                    showDialog(view);
+                }else{
                 shelterList.addHashMap(ParseUtilities.loadJSON(localFileLocation));
                 ((ShelterTrackerApplication)getApplication()).writeFile();
+                }
             }
         });
+
 
 //        ListView lv = findViewById(R.id.shelter_list);
 
 //        lv.setAdapter(new ShelterAdapter(this,
 //                (List<Shelter>) ((ShelterTrackerApplication)getApplication()).getShelterList().getShelters()));
 //
+    }
+    public void showDialog(View view) {
+
+        Dialog dialog = new AlertDialog.Builder(this).setTitle("My alert").setCancelable(false)
+                .setMessage("File does not exist")
+                .setPositiveButton( "OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                }).create();
+
+        dialog.show();
     }
 
 
